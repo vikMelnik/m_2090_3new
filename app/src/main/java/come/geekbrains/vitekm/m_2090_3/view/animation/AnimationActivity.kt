9cce86e7ddb2b2113632f86cdb461e33b1e71402
ViktorMel <vitekm@ukr.net>
2022-07-13 13:13:32 +0300

    Lesson 5 Example with tracking

--- app/src/main/java/come/geekbrains/vitekm/m_2090_3/view/animation/AnimationActivity.kt
+++ app/src/main/java/come/geekbrains/vitekm/m_2090_3/view/animation/AnimationActivity.kt
@@ -2,9 +2,11 @@ package come.geekbrains.vitekm.m_2090_3.view.animation
 
 import android.graphics.Rect
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -12,47 +14,73 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.*
 import come.geekbrains.vitekm.m_2090_3.R
 import come.geekbrains.vitekm.m_2090_3.databinding.ActivityAnimationBinding
+import come.geekbrains.vitekm.m_2090_3.databinding.ActivityAnimationTreckBinding
 
 
 class AnimationActivity: AppCompatActivity() {
 
-    private lateinit var binding: ActivityAnimationBinding
-    var isFlag = false
+    private lateinit var binding: ActivityAnimationTreckBinding
+    var isFlag = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAnimationBinding.inflate(layoutInflater)
+        binding = ActivityAnimationTreckBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.imageView.setOnClickListener{
-            isFlag = !isFlag
-
-            val params = it.layoutParams as ConstraintLayout.LayoutParams
-
-            val transitionSet = TransitionSet()
-            val changeImageTransform = ChangeImageTransform()
-            val changeBounds = ChangeBounds()
-            changeImageTransform.duration = 2000L
-            changeBounds.duration = 2000L
-
-            transitionSet.addTransition(changeBounds)           // Важен порядок, changeImageTransform после changeBounds
-            transitionSet.addTransition(changeImageTransform)
-
-
-            TransitionManager.beginDelayedTransition(binding.root, transitionSet)
-
-            if (isFlag){
-                params.height = ConstraintLayout.LayoutParams.MATCH_PARENT
-                binding.imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-            }else{
-                params.height = ConstraintLayout.LayoutParams.WRAP_CONTENT
-                binding.imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
-            }
-
-            it.layoutParams = params    //Иногда работает без нее
-        }
+        // 3) Tracking
+//        binding.button.setOnClickListener{
+//            isFlag = !isFlag
+//
+//            val params = it.layoutParams as FrameLayout.LayoutParams
+//
+//            val changeBounds = ChangeBounds()
+//            changeBounds.duration = 2000L
+//            changeBounds.setPathMotion(ArcMotion())
+//
+//            TransitionManager.beginDelayedTransition(binding.root, changeBounds)
+//
+//            if (isFlag){
+//                params.gravity = Gravity.TOP or Gravity.START
+//
+//            }else{
+//                params.gravity = Gravity.BOTTOM or Gravity.END
+//
+//            }
+//
+//            it.layoutParams = params    //Иногда работает без нее
+//        }
 
 
+        // 2) Increase objects
+//        binding.imageView.setOnClickListener{
+//            isFlag = !isFlag
+//
+//            val params = it.layoutParams as ConstraintLayout.LayoutParams
+//
+//            val transitionSet = TransitionSet()
+//            val changeImageTransform = ChangeImageTransform()
+//            val changeBounds = ChangeBounds()
+//            changeImageTransform.duration = 2000L
+//            changeBounds.duration = 2000L
+//
+//            transitionSet.addTransition(changeBounds)           // Важен порядок, changeImageTransform после changeBounds
+//            transitionSet.addTransition(changeImageTransform)
+//
+//
+//            TransitionManager.beginDelayedTransition(binding.root, transitionSet)
+//
+//            if (isFlag){
+//                params.height = ConstraintLayout.LayoutParams.MATCH_PARENT
+//                binding.imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+//            }else{
+//                params.height = ConstraintLayout.LayoutParams.WRAP_CONTENT
+//                binding.imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+//            }
+//
+//            it.layoutParams = params    //Иногда работает без нее
+//        }
+
+        // 1) Transition
 //        binding.button.setOnClickListener{
 //            isFlag = !isFlag
 //
