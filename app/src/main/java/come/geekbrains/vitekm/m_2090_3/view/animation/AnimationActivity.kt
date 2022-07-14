356abe9be6ea2b0bf2eb67b8787df89977ab4561
ViktorMel <vitekm@ukr.net>
2022-07-14 16:11:32 +0300

    Lesson 5 Example with ConstraintSet

--- app/src/main/java/come/geekbrains/vitekm/m_2090_3/view/animation/AnimationActivity.kt
+++ app/src/main/java/come/geekbrains/vitekm/m_2090_3/view/animation/AnimationActivity.kt
@@ -1,97 +1,49 @@
 package come.geekbrains.vitekm.m_2090_3.view.animation
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.ObjectAnimator
-import android.graphics.Rect
 import android.os.Bundle
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.animation.AnticipateOvershootInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.ViewCompat
-import androidx.recyclerview.widget.RecyclerView
-import androidx.transition.*
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.transition.ChangeBounds
+import androidx.transition.TransitionManager
 import come.geekbrains.vitekm.m_2090_3.R
 import come.geekbrains.vitekm.m_2090_3.databinding.*
 
 
 class AnimationActivity: AppCompatActivity() {
 
-    private lateinit var binding: ActivityAnimationsFabScrolBinding
+    private lateinit var binding: ActivityAnimationsBonusStartBinding
     var isFlag = false
     private val duration = 2000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAnimationsFabScrolBinding.inflate(layoutInflater)
+        binding = ActivityAnimationsBonusStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.scrollView.setOnScrollChangeListener { _, _, _, _, _ ->
-            binding.toolbar.isSelected = binding.scrollView.canScrollVertically(-1)
-        }
+        val constraintSetStart = ConstraintSet()
+        val constraintSetEnd = ConstraintSet()
+        //constraintSetStart.clone(binding.constraintContainer)
+        constraintSetStart.clone(this, R.layout.activity_animations_bonus_start)
+        constraintSetEnd.clone(this, R.layout.activity_animations_bonus_end)
 
-        // 4) Mix
-        val titles: MutableList<String> = ArrayList()
-        for (i in 0..4) {
-            titles.add(String.format("Item %d", i + 1))
-        }
-
-        binding.fab.setOnClickListener {
+        binding.backgroundImage.setOnClickListener{
             isFlag = !isFlag
 
-            if(isFlag){
-                ObjectAnimator.ofFloat(binding.plusImageview, View.ROTATION, 0f, 675f).setDuration(duration).start()
-                ObjectAnimator.ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, -130f).setDuration(duration).start()
-                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, -230f).setDuration(duration).start()
+            val transition =  ChangeBounds()
+            transition.interpolator = AnticipateOvershootInterpolator(5.0f)
+            transition.duration = 1200
+            TransitionManager.beginDelayedTransition(binding.constraintContainer, transition)
 
-                ObjectAnimator.ofFloat(binding.transparentBackground, View.ALPHA, 0f).setDuration(duration).start()
-
-                binding.optionOneContainer.animate().alpha(1f).setDuration(duration).setListener(
-                    object : AnimatorListenerAdapter(){
-                        override fun onAnimationEnd(animation: Animator?) {
-                            binding.optionOneContainer.isClickable = true
-                        }
-                    }
-                )
-                binding.optionTwoContainer.animate().alpha(1f).setDuration(duration).setListener(
-                    object : AnimatorListenerAdapter(){
-                        override fun onAnimationEnd(animation: Animator?) {
-                            binding.optionTwoContainer.isClickable = true
-                        }
-                    }
-                )
-
+            if (isFlag){
+                constraintSetEnd.applyTo(binding.constraintContainer)
             }else{
-                ObjectAnimator.ofFloat(binding.plusImageview, View.ROTATION, 675f, 0f).setDuration(duration).start()
-                ObjectAnimator.ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, 0f).setDuration(duration).start()
-                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, 0f).setDuration(duration).start()
-
-                ObjectAnimator.ofFloat(binding.transparentBackground, View.ALPHA, 0.8f).setDuration(duration).start()
-
-                binding.optionOneContainer.animate().alpha(0f).setDuration(duration).setListener(
-                    object : AnimatorListenerAdapter(){
-                        override fun onAnimationEnd(animation: Animator?) {
-                            binding.optionOneContainer.isClickable = false
-                        }
-                    }
-                )
-                binding.optionTwoContainer.animate().alpha(0f).setDuration(duration).setListener(
-                    object : AnimatorListenerAdapter(){
-                        override fun onAnimationEnd(animation: Animator?) {
-                            binding.optionTwoContainer.isClickable = false
-                        }
-                    }
-                )
+                constraintSetStart.applyTo(binding.constraintContainer)
             }
-
         }
     }
 }
+
+
 
 
